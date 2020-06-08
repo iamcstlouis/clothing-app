106939c8cff2e86fdb2a276e8629c0cc3390c631
Chantal St Louis <chantalstlouis@akauk.com>
2020-06-08 18:07:02 +0100

    Adds shop component page

--- src/App.js
+++ src/App.js
@@ -4,12 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import Homepage from './pages/homepage/homepage.component';
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-);
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
@@ -17,7 +12,7 @@ function App() {
       {/* <Homepage /> */}
       <Switch>
         <Route exact path='/' component={Homepage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
