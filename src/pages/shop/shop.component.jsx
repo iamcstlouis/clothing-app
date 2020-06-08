b2ea599e33d4f563d48e4797060b7906bc43c6ea
Chantal St Louis <chantalstlouis@akauk.com>
2020-06-08 22:47:47 +0100

    Adds the collection-preview component, styles and shop page

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import SHOP_DATA from './shop.data';
 
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+
 class ShopPage extends React.Component {
     constructor(props) {
         super(props);
@@ -12,9 +14,14 @@ class ShopPage extends React.Component {
     }
 
     render() {
+        const { collections } = this.state;
         return (
-            <div>
-                SHOP PAGE
+            <div className='shop-page'>
+                {
+                    collections.map(({ id, ...otherCollectionProps }) => (
+                        <CollectionPreview key={id} {...otherCollectionProps} />
+                    ))
+                }
             </div>
         );
     }
